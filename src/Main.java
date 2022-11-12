c6067fbf29f808a1c784e1ee38e1fa3acff083d1
THONWELLING <thonwellingdani13@gmail.com>
2022-11-12 00:25:25 -0300

    Criando as classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
   public static void main(String[] args) {
+
     System.out.println("Hello world!");
+
+    Curso curso1 = new Curso();
+    curso1.setTitulo("Curso Java");
+    curso1.setDescricao("Descrição do curso de Java");
+    curso1.setCargaHoraria(90);
+
+    Curso curso2 = new Curso();
+    curso2.setTitulo("Curso Nodejs");
+    curso2.setDescricao("Descrição do curso de Nodejs");
+    curso2.setCargaHoraria(70);
+
+
+    Mentoria mentoria1 = new Mentoria();
+    mentoria1.setTitulo("Mentoria Java");
+    mentoria1.setDescricao("Descrição mentoria Java");
+    mentoria1.setData(LocalDate.now());
+
+    System.out.println(curso1);
+    System.out.println(curso2);
+    System.out.println(mentoria1);
   }
 }
